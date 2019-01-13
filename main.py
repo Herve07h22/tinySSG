455a3d101512014589ca10a32316cfda5025c499
Herve07h22 <contact@storeed.me>
2019-01-13 10:40:46 +0100

    added exif auto rotation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os.path import join
 from os import getcwd
 
 if __name__ =='__main__':
-    mon_site = ssg(contentDir=join(getcwd(), "content"))
+    mon_site = ssg(contentDir=join(getcwd(), "content"), exif=False)
     mon_site.addImageProcessing( label='dimension400x200', x=400, y=200  )
     mon_site.addImageProcessing( label='square', x=400, y=400  )
     mon_site.renderAssets( assetDir=join(getcwd(), "assets") , destination_dir=join(getcwd(), "dist") )
